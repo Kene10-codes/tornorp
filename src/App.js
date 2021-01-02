0714bbe6b929c91ca6bf52d8afb208a8c5d4b87f
Kenechukwu Nwobodo <nwobodokenechukwu2@gmail.com>
2021-01-02 13:36:35 -0500

    Started off with this application - install all necessary packages - install react-router-dom - install formik - install  react-bootstrap - set up the router file and file structure

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Suspense } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ErrorBoundary from './pages/error/ErrorBoundary'
+import './App.css';
+
+const Home = React.lazy(() => import('./pages/home/Home'))
+const LoginPage = React.lazy(() => import('./pages/login/LoginPage'))
+const SignupPage = React.lazy(() => import('./pages/signup/SignupPage'))
+
+
+
+function App() {
+  return (
+    <ErrorBoundary>
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+             <Home exact path='/' component={Home} />
+             <SignupPage path='/SignupPage' component={SignupPage} />
+             <LoginPage path='/LoginPage' component={LoginPage} />
+        </Switch>
+      </Suspense>
+    </Router>
+    </ErrorBoundary>
+  );
+}
+
+export default App;
